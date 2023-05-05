36cf7709330b08e7047f8349e2a4e6fff83a0413
VinCode91 <vincenzatto@gmail.com>
2023-05-05 20:12:13 +0200

    persist image in DB, unit test use MockMultipartFile

--- src/main/java/com/example/sfgtrainingrecipe/services/ImageServiceImpl.java
+++ src/main/java/com/example/sfgtrainingrecipe/services/ImageServiceImpl.java
@@ -1,16 +1,44 @@
 package com.example.sfgtrainingrecipe.services;
 
+import com.example.sfgtrainingrecipe.domain.Recipe;
+import com.example.sfgtrainingrecipe.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Service
 @Slf4j
 public class ImageServiceImpl implements ImageService{
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
 
-        log.debug("Received a file");
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
 
     }
 }
